f68bc52635b55217fbe5ec8c3d3d5a228a4e6b6a
widberg <jaredwidberg@gmail.com>
2021-06-06 18:47:10 -0400

    surface datas

--- src/fuel_dpc.rs
+++ src/fuel_dpc.rs
@@ -1414,6 +1414,7 @@ impl DPC for FuelDPC {
         fmt_fns.insert(2204276779, fuel_fmt::fuel_fmt_extract_material_z);
         fmt_fns.insert(1391959958, fuel_fmt::fuel_fmt_extract_user_define_z);
         fmt_fns.insert(4096629181, fuel_fmt::fuel_fmt_extract_game_obj_z);
+        fmt_fns.insert(3747817665, fuel_fmt::fuel_fmt_extract_surface_datas_z);
 
 		fs::create_dir_all(output_path)?;
 
--- src/fuel_fmt.rs
+++ src/fuel_fmt.rs
@@ -156,3 +156,40 @@ pub fn fuel_fmt_extract_game_obj_z(header: &[u8], data: &[u8], output_path: &Pat
 
 	Ok(())
 }
+
+
+#[derive(Serialize, Deserialize, NomLE)]
+#[nom(Exact)]
+struct SurfaceDatasZ {
+	one: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SurfaceDatasObject {
+	resource_object: ResourceObjectZ,
+	surface_datas: SurfaceDatasZ,
+}
+
+pub fn fuel_fmt_extract_surface_datas_z(header: &[u8], data: &[u8], output_path: &Path) -> Result<()> {
+	let json_path = output_path.join("object.json");
+	let mut output_file = File::create(json_path)?;
+
+	let resource_object = match ResourceObjectZ::parse(&header) {
+		Ok((_, h)) => h,
+		Err(error) => panic!("{}", error),
+	};
+
+	let surface_datas = match SurfaceDatasZ::parse(&data) {
+		Ok((_, h)) => h,
+		Err(error) => panic!("{}", error),
+	};
+
+	let material_object = SurfaceDatasObject {
+		resource_object,
+		surface_datas,
+	};
+
+	output_file.write(serde_json::to_string_pretty(&material_object)?.as_bytes())?;
+
+	Ok(())
+}
